84aa2dccb09381f3011736242f97b61efb969c12
aditasyhari <adit.asyhari@gmail.com>
2022-01-28 03:05:56 +0700

    commit

--- resources/views/backend/layouts/sidebar.blade.php
+++ resources/views/backend/layouts/sidebar.blade.php
@@ -10,44 +10,17 @@
         <li class="dropdown active">
             <a href="{{ url('/') }}" class="nav-link"><i data-feather="monitor"></i><span>Dashboard</span></a>
         </li>
+        <li><a class="nav-link" href="#"><i data-feather="user"></i><span>List Pesanan</span></a></li>
         <li class="dropdown">
-            <a href="#" class="menu-toggle nav-link has-dropdown"><i
-                data-feather="briefcase"></i><span>Widgets</span></a>
+            <a href="#" class="menu-toggle nav-link has-dropdown"><i data-feather="file"></i><span>Laporan</span></a>
             <ul class="dropdown-menu">
-            <li><a class="nav-link" href="widget-chart.html">Chart Widgets</a></li>
-            <li><a class="nav-link" href="widget-data.html">Data Widgets</a></li>
+            <li><a class="nav-link" href="#">Keuangan</a></li>
+            <li><a class="nav-link" href="#">Pesanan</a></li>
             </ul>
         </li>
-        <li class="dropdown">
-            <a href="#" class="menu-toggle nav-link has-dropdown"><i data-feather="command"></i><span>Apps</span></a>
-            <ul class="dropdown-menu">
-            <li><a class="nav-link" href="chat.html">Chat</a></li>
-            <li><a class="nav-link" href="portfolio.html">Portfolio</a></li>
-            <li><a class="nav-link" href="blog.html">Blog</a></li>
-            <li><a class="nav-link" href="calendar.html">Calendar</a></li>
-            </ul>
-        </li>
-        <li class="dropdown">
-            <a href="#" class="menu-toggle nav-link has-dropdown"><i data-feather="mail"></i><span>Email</span></a>
-            <ul class="dropdown-menu">
-            <li><a class="nav-link" href="#">Inbox</a></li>
-            <li><a class="nav-link" href="#">Compose</a></li>
-            <li><a class="nav-link" href="#">read</a></li>
-            </ul>
-        </li>
-        <li class="menu-header">UI Elements</li>
-        <li><a class="nav-link" href="blank.html"><i data-feather="file"></i><span>Blank Page</span></a></li>
-        <li class="menu-header">Otika</li>
-        <li class="dropdown">
-            <a href="#" class="menu-toggle nav-link has-dropdown"><i data-feather="layout"></i><span>Forms</span></a>
-            <ul class="dropdown-menu">
-            <li><a class="nav-link" href="basic-form.html">Basic Form</a></li>
-            <li><a class="nav-link" href="forms-advanced-form.html">Advanced Form</a></li>
-            <li><a class="nav-link" href="forms-editor.html">Editor</a></li>
-            <li><a class="nav-link" href="forms-validation.html">Validation</a></li>
-            <li><a class="nav-link" href="form-wizard.html">Form Wizard</a></li>
-            </ul>
-        </li>
+        <li class="menu-header">Data User</li>
+        <li><a class="nav-link" href="#"><i data-feather="user"></i><span>Customer</span></a></li>
+        <li><a class="nav-link" href="#"><i data-feather="user"></i><span>Kurir</span></a></li>
         </ul>
     </aside>
 </div>
